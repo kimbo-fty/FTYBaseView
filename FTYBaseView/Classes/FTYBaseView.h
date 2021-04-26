b56ca403bd8b173e26d5e4942cab15f95dda8523
kimbo <kimbo_slice@163.com>
2021-04-26 15:09:30 +0800

    init project

---
+++ FTYBaseView/Classes/FTYBaseView.h
@@ -0,0 +1,16 @@
+//
+//  FTYBaseView.h
+//  FTYBaseView
+//
+//  Created by kimbo on 2021/4/26.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FTYBaseView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FTYBaseView/Classes/FTYBaseView.m
@@ -0,0 +1,20 @@
+//
+//  FTYBaseView.m
+//  FTYBaseView
+//
+//  Created by kimbo on 2021/4/26.
+//
+
+#import "FTYBaseView.h"
+
+@implementation FTYBaseView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
